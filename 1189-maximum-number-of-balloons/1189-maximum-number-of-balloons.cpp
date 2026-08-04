c35fd6451f961b2fdf8919d859ac640d70468370
Shivanshu singh <152587989+0shivanshuu@users.noreply.github.com>
2026-08-04 12:35:26 +0530

    Time: 0 ms (100%), Space: 9.1 MB (21.47%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        unordered_map<int,int> mp;
+
+        for(int i: text){
+            mp[i]++;       
+        }
+
+        int ans=min({mp['b'],mp['a'],mp['l']/2,mp['o']/2,mp['n']});
+
+        return ans;
+    }
+};
