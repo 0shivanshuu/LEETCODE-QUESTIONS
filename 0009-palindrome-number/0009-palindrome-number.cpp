bc5ca13756d9d8d0b6efd215a60a9d659da7e686
Shivanshu singh <152587989+0shivanshuu@users.noreply.github.com>
2026-09-05 00:36:30 +0530

    Time: 4 ms (30.55%), Space: 8.5 MB (92.06%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
+    int reverse(int n){
+        long long ans=0;
+        while(n>0){
+            int rem=n%10;
+            ans=ans*10+rem;
+            n=n/10;
+
+        }
+        return ans;
+    }
+
     bool isPalindrome(int x) {
         if(x<0){
             return false;
         }
 
-        string s= to_string(x);
+        // string s= to_string(x);
 
-        int i=0;
-        int j=s.size()-1;
+        // int i=0;
+        // int j=s.size()-1;
 
-        while(i<j){
-            if(s[i]!=s[j]){
-                return false;
-            }
-            i++;
-            j--;
+        // while(i<j){
+        //     if(s[i]!=s[j]){
+        //         return false;
+        //     }
+        //     i++;
+        //     j--;
+        // }
+
+        if(x==reverse(x)){
+            return true;
         }
 
-        return true;
+        else{
+            return false;
+        }
 
         
     }
